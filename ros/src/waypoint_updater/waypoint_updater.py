c1b3f0da04cb921434841fb1570278bf413ca66d
Seo, Jungwoo <36559904+imisu9@users.noreply.github.com>
2019-05-19 08:11:09 +0900

    fix: typo correction

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -53,7 +53,7 @@ class WaypointUpdater(object):
                 self.publish_waypoints(closest_waypoint_idx)
             rate.sleep()
         
-    def get_closest_waypont_idx(self):
+    def get_closest_waypoint_idx(self):
         x = self.pose.pose.position.x
         y = self.pose.pose.position.y
         closest_idx = self.waypoint_tree.query([x, y], 1)[1]
